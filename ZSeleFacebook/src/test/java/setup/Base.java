a9d390f0e554e1a6d19bef9bacab82c799f7bab1
Vaishnavi <Vaishnavi@DESKTOP-HF3IB17>
2023-03-28 12:55:30 -0700

    second commit opera browser added

--- ZSeleFacebook/src/test/java/setup/Base.java
+++ ZSeleFacebook/src/test/java/setup/Base.java
@@ -3,6 +3,7 @@ package setup;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class Base {
 
@@ -18,6 +19,11 @@ public class Base {
 		 return driver;
 		
 	}
+	public static WebDriver openOperaBrowser() {
+		 System.setProperty("webdriver.gecko.driver", "C:\\Users\\Saurabh Zend\\Desktop\\selinum\\geckodriver.exe"); 
+		 WebDriver driver = new OperaDriver();
+		 return driver;
+		
 	//public static WebDriver openFireFoxBrowser() {
 		 //System.setProperty("webdriver.gecko.driver", "C:\\Users\\Saurabh Zend\\Desktop\\selinum\\geckodriver.exe"); 
 		// WebDriver driver = new FirefoxDriver();
